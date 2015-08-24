b66b86b38bf19bc79904f33b13ad74e322629a60
Brett Trani <brett.trani@hotmail.com>
2015-08-24 12:57:32 -0700

    Update XGB model.

--- Kaggle/Liberty_Mutual/liberty_mutual_RF.py
+++ Kaggle/Liberty_Mutual/liberty_mutual_RF.py
@@ -44,43 +44,46 @@ train = train.astype(float)
 test = test.astype(float)
 
 #Model #1 RandomForestRegressor tuned using GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
-from sklearn import cross_validation
-from sklearn.grid_search import GridSearchCV
+#from sklearn.ensemble import RandomForestRegressor
+#from sklearn import cross_validation
+#from sklearn.grid_search import GridSearchCV
 
 #Split data into train and test
-#X_train, X_test, y_train, y_test = train_test_split(train, labels, \
+#X_train, X_test, y_train, y_test = cross_validation.train_test_split(train, labels, \
 #test_size=0.2, random_state=0)
 
 #Determine parameters for grid search
-param_grid = {'n_estimators': [1000], 'max_depth': [10,20], \
-'min_samples_leaf': [5], 'n_jobs' : [-1]}
+#param_grid = {'n_estimators': [1000,2000], 'max_depth': [10,20], \
+#'min_samples_leaf': [5,8], 'n_jobs' : [-1]}
 
 #Use KFold cross validation
-cv = cross_validation.KFold(len(train), n_folds = 10)
+#cv = cross_validation.KFold(len(train), n_folds = 10)
 
 #Train and fit RF model
-model = GridSearchCV(RandomForestRegressor(), param_grid, cv=cv)
-model = model.fit(train, labels)
+#model = RandomForestRegressor(n_estimators=500, max_depth = 10)
+#model = model.fit(X_train, y_train)
+#score = model.score(X_test, y_test)
+
+#print score
 
 #Use trained model to predict test values
-predict = model.predict(test)
+#rf_predict = model.predict(test)
 
 #Send predicted scores to csv file
-submission = pd.DataFrame({"Id": test_ind, "Hazard": predict})
-submission = submission.set_index("Id")
-submission.to_csv('/Users/btrani/Git/projects/Kaggle/Liberty_Mutual/sub_8.csv')
+#submission = pd.DataFrame({"Id": test_ind, "Hazard": predict})
+#submission = submission.set_index("Id")
+#submission.to_csv('/Users/btrani/Git/projects/Kaggle/Liberty_Mutual/sub_8.csv')
 
 #Model #2 Extreme Gradient Boost using xgboost
 import xgboost as xgb
 
 #Subset the data and set up model parameters
 offset = 5000
-num_round = 1000
+num_round = 500
 xgtest = xgb.DMatrix(test)
-gb_params = {"objective":"reg:linear", "eta": 0.005, "min_child_weight": 6, \
+gb_params = {"objective":"reg:linear", "eta": 0.01, "min_child_weight": 6, \
 "subsample": 0.7, "colsample_bytree": 0.7, "scale_pos_weight": 1, "silent": 1, 
-"max_depth": 12}
+"max_depth": 8}
 
 #Create a train and validation dmatrices 
 xgtrain = xgb.DMatrix(train[offset:,:], label=labels[offset:])
@@ -90,13 +93,13 @@ xgval = xgb.DMatrix(train[:offset,:], label=labels[:offset])
 watchlist = [(xgtrain, 'train'),(xgval, 'val')]
 model = xgb.train(gb_params, xgtrain, num_round, watchlist, \
 early_stopping_rounds=4)
-preds1 = model.predict(xgtest)
+xg_preds = model.predict(xgtest, ntree_limit=model.best_iteration)
 
 fscore = [ (v,k) for k,v in model.get_fscore().iteritems() ]
 fscore.sort(reverse=True)
 
 #Send predicted scores to csv file
-submission = pd.DataFrame({"Id": test_ind, "Hazard": preds1})
+submission = pd.DataFrame({"Id": test_ind, "Hazard": xg_preds})
 submission = submission.set_index("Id")
-submission.to_csv('/Users/btrani/Git/projects/Kaggle/Liberty_Mutual/sub_gb_3.csv')
+submission.to_csv('/Users/btrani/Git/projects/Kaggle/Liberty_Mutual/sub_gb_7.csv')
 
